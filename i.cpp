348684a69e4c4717a810a5e9017adf74d560f292
ihsan97 <40269169+ihsan97@users.noreply.github.com>
2018-06-17 17:40:26 +0530

    Add files via upload

---
+++ i.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+	printf("University of Moratuwa\n");
+	printf("Katubedda,\n");
+	printf("Moratwa,\n");
+	printf("Sri Lanka\n");
+	printf("-----------------\n");
+	printf("www.mrt.ac.lk");
+}
