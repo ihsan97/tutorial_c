705b20f13d6b6bf2546bd052eafc347bacc18548
ihsan97 <40269169+ihsan97@users.noreply.github.com>
2018-06-26 13:53:33 +0530

    Add files via upload

---
+++ program 2.7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,sum;
+    printf("Enter the first number =\n");
+    scanf("%f",&a);
+    printf("Enter the second number =\n");
+    scanf("%f",&b);
+    sum=a+b;
+    printf("Answer=%.2f",sum);
+    return 0;
+}
---
+++ program2.2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+float number = 34.5678;
+printf("A is %.0f\n",number);
+printf("B is %.2f\n",number);
+return 0;
+ }
